ac4c46cfeb34ea0d2793cae9dc34e835bc5e2f72
wallina99 <awwallin63@gmail.com>
2017-04-04 14:48:39 -0700

    Update lab1.sh

--- lab1.sh
+++ lab1.sh
@@ -1,7 +1,7 @@
 
 # Orientation Notes
-pwd     # print working directory    tells where I am
-whami   # tells you which user you're logged in as
-ls      # list  tells what files and directories are at this location
+pwd     # print working directory - tells where I am
+whami   # who am i? - tells you which user you're logged in as
+ls      # list - tells what files and directories are at this location
 cd      # change directory - allows navigation into other directories
 date    # gives the time on the machine - quite important for networking
