57d54a5c7e032c21037d3572338e0eb3b5c0241d
wallina99 <awwallin63@gmail.com>
2017-04-04 15:11:37 -0700

    Update lab1.sh

--- lab1.sh
+++ lab1.sh
@@ -5,3 +5,8 @@ whami   # who am i? - tells you which user you're logged in as
 ls      # list - tells what files and directories are at this location
 cd      # change directory - allows navigation into other directories
 date    # gives the time on the machine - quite important for networking
+
+#created a file using the linux run thru a browser and the vi editor:
+/var/root # cat example.sh                                                      
+                                                                                
+echo "this is a class" 
